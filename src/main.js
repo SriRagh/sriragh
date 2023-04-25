04206d6dc1e58a6dc0cab1be1e7ccc9320df7f3e
Sri Ragh Chilukamukku <sriragh@floatteam.com>
2023-04-25 17:12:36 +0530

    github

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 console.log(import.meta.env, window, "env")
-console.log(window.process)
 import './assets/main.css'
 
 const app = createApp(App)
